89ae02e5b2ae281e1c89a72300f28c8c695ad61f
Kuparinen Simo A <simokupa@da2-ex-dk107-18.helsinki.fi>
2017-11-09 00:15:48 +0200

    readme päivitetty

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,26 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+    
+    public static function drinks_edit(){
+        View::make('suunnitelmat/drinks_edit.html');
+    }
+    
+    public static function drinks_list(){
+        View::make('suunnitelmat/drinks_list.html');
+    }
+    
+    public static function drinks_show(){
+        View::make('suunnitelmat/drinks_show.html');
+    }
+    
+    public static function login(){
+        View::make('suunnitelmat/login.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,19 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/drinks', function() {
+    HelloWorldController::drinks_list();
+  });
+  
+  $routes->get('/drinks/1', function() {
+    HelloWorldController::drinks_show();
+  });
+  
+  $routes->get('/drinks/edit', function() {
+    HelloWorldController::drinks_edit();
+  });
+  
+  $routes->get('/login', function() {
+    HelloWorldController::login();
+  });
